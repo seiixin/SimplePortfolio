6eab82c3ff9675ad612b7d1857c2861772686ad5
Seiixin <sidneygarcinan@gmail.com>
2025-06-13 03:33:59 +0800

    updated skills

--- script.js
+++ script.js
@@ -74,12 +74,7 @@ const categoryIcons = {
     </svg>`,
     "Tools": `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>`,
-      "Methodologies & Architecture":`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4 4H20V20H4V4Z" stroke="currentColor" stroke-width="2"/>
-      <path d="M4 9H20" stroke="currentColor" stroke-width="2"/>
-      <path d="M9 4V20" stroke="currentColor" stroke-width="2"/>
-  </svg>`
+    </svg>`
 };
 
 // SVG Icons for individual skills
